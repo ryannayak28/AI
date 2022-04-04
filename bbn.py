6ac8e9cf236fe6a41fbd149ea1d0018609e9d23c
Ryan Nayak <nayakryan@gmail.com>
2022-04-05 00:40:09 +0530

    updated functionality to generate events & statically added Conditional Probabilities

--- bbn.py
+++ bbn.py
@@ -9,7 +9,7 @@ def generateIndependentNodes(DAG):
     return ind_events
 
 
-def generateDependentNodes(DAG):
+def generateDependentNodes(DAG, ind_events):
     flat_dep_events = [num for sublist in list(DAG.values()) for num in sublist]
 
     cond_ind_events = list()
@@ -25,8 +25,22 @@ def generateDependentNodes(DAG):
         # removing the duplicate value(s) from the unique list
         if event in cond_ind_events:
             dep_events.remove(event)
+    
+    cond_ind_event_temp = list()
+    # updates cond_ind_event
+    for event in ind_events:
+        for c_event in cond_ind_events:
+            new_event = c_event + "|" + event
+            cond_ind_event_temp.append(new_event)
 
-    return cond_ind_events, dep_events
+    dep_event_temp = list()
+    # updates dep_event
+    for event in cond_ind_events:
+        for d_event in dep_events:
+            new_event = d_event + "|" + event
+            dep_event_temp.append(new_event)
+
+    return cond_ind_event_temp, dep_event_temp
         
 
 def computeIEConditionalProbability(events):
@@ -42,23 +56,68 @@ def computeIEConditionalProbability(events):
     return event_cp
 
 
+def generateDependentEvents(ind_events, dep_events, cond_ind_events):
+    # ind_events => {'B', 'E'}, dep_event => ['DC', 'SC'], cond_ind_events => ['A']
+    # dep_event => ['DC|A', 'SC|A'], cond_ind_events => ['A|B', 'A|E']
+    cond_ind_event_temp = list()
+    # updates cond_ind_event
+    for event in ind_events:
+        for c_event in cond_ind_events:
+            new_event = c_event + "|" + event
+            cond_ind_event_temp.append(new_event)
+    print(cond_ind_event_temp)
+
+    dep_event_temp = list()
+    # updates dep_event
+    for event in cond_ind_events:
+        for d_event in dep_events:
+            new_event = d_event + "|" + event
+            dep_event_temp.append(new_event)
+    print(dep_event_temp)
+
+
 def computeCIEConditionalProbability(cond_ind_events):
-    pass
+    temp_cp = {
+        'A|B^E':0.94,
+        'A|B^~E':0.95,
+        'A|~B^E':0.69,
+        'A|~B^~E':0.999,
+        '~A|B^E':0.06,
+        '~A|B^~E':0.05,
+        '~A|~B^E':0.31,
+        '~A|~B^~E':0.001,
+    }
+    return temp_cp
+    
 
 
 def computeDEConditionalProbability(dep_events):
-    pass
+    temp_cp = {
+        'DC|A': 0.91,
+        'DC|~A':0.05, 
+        '~DC|A': 0.91,
+        '~DC|~A':0.05,
+        'SC|A': 0.75,
+        'SC|~A':0.02, 
+        '~SC|A': 0.25,
+        '~SC|~A':0.98,
+    }
+    return temp_cp
 
 
 def main():
     ind_events = generateIndependentNodes(DAG)
-    cp_ind_events = computeIEConditionalProbability(ind_events)
+    # cp_ind_events = computeIEConditionalProbability(ind_events)
     
-    cond_ind_events, dep_events = generateDependentNodes(DAG)
-    print(f"Conditionally Independent Events : {cond_ind_events}")
-    print(f"Dependent Events : {dep_events}")
+    cond_ind_events, dep_events = generateDependentNodes(DAG, ind_events)
+    print(f"\nConditionally Independent Events : {cond_ind_events}")
+    print(f"\nDependent Events : {dep_events}")
+
     cp_cond_ind_events = computeCIEConditionalProbability(cond_ind_events)
+    print(f"\nC.P. for CI Events : {cp_cond_ind_events}")
     cp_dep_events = computeDEConditionalProbability(dep_events)
+    print(f"\nC.P. for D Events : {cp_dep_events}")
+    
     # TO DO: Update the compute cp functions for conditionally independent & dependent events 
     # each containing different inputs required for the truth tables
 
